4e1fd8a563fe36096afa8ade42cb396402fa0fe5
Pedro Arvela <pedro.arvela@onfido.com>
2020-12-01 14:35:47 +0000

    Remove duplicate

--- day_1/__main__.py
+++ day_1/__main__.py
@@ -1,15 +1,5 @@
 from typing import List
-
-def day1(expense: List[int]) -> int:
-    desired_sum = 2020
-
-    for i, v1 in enumerate(expense):
-        for v2 in expense[i:]:
-            if v1 + v2 == desired_sum:
-                return v1 * v2
-
-    return -1
-
+from solution import day1
 
 def main():
     with open('inputs/day1.txt', 'r') as f:
